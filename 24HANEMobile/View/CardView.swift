3dda4e69ac4145ff1a4ccbc7ecea5cafe1d7ab55
Katherine <01kathyy@gmail.com>
2023-02-01 18:46:59 +0900

    cardView

---
+++ 24HANEMobile/View/CardView.swift
@@ -0,0 +1,22 @@
+//
+//  CardView.swift
+//  24HANEMobile
+//
+//  Created by Katherine JANG on 2/1/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ 24HANEMobile/View/ProgressCircleView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressCircleView.swift
+//  24HANEMobile
+//
+//  Created by Katherine JANG on 2/1/23.
+//
+
+import SwiftUI
+
+struct ProgressCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressCircleView()
+    }
+}
